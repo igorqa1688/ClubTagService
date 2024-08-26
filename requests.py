4422dc81c0904518c399d448c83aa9c14c4578ef
leviafan14 <14leviafan88@gmail.com>
2024-08-26 08:34:03 +0500

    Write create tests

--- requests.py
+++ requests.py
@@ -5,11 +5,12 @@ from functions import generate_guid, generate_random_string
 from global_vars import server
 
 
-def create_club_tag(club_id: str, name: str, color: str):
+def create_club_tag(club_guid: str, name: str, color: str):
     with grpc.insecure_channel(server) as channel:
         stub = tag_service_pb2_grpc.TagServiceStub(channel)
+
         request = tag_service_pb2.CreateClubTagRequest(
-            club_id=club_id,
+            club_guid=tag_service_pb2.GUID(value=f"{club_guid}"),
             name=name,
             color=color)
         try:
@@ -20,8 +21,27 @@ def create_club_tag(club_id: str, name: str, color: str):
             return "Error create_club_tag()"
 
 
+def update_club_tag(guid: str, name: str, color: str):
+    with grpc.insecure_channel(server) as channel:
+        stub = tag_service_pb2_grpc.TagServiceStub(channel)
+
+        request = tag_service_pb2.UpdateClubTagRequest(
+            guid=tag_service_pb2.GUID(value=f"{guid}"),
+            name=name,
+            color=color)
+        try:
+            response = stub.UpdateClubTag(request)
+            return response
+        except Exception as e:
+            print(e)
+            return "Error update_club_tag()"
+
+
 if __name__ == "__main__":
-    club_id = generate_guid()
+    club_guid = generate_guid()
     name = generate_random_string(12)
     color = generate_random_string(5)
-    create_club_tag(club_id, name, color)
+    created_tag = create_club_tag(club_guid, name, color)
+    created_tag_guid = created_tag.tag.guid.value
+    print(created_tag_guid)
+    print(update_club_tag(created_tag_guid, name, color))
--- test_tags_service.py
+++ test_tags_service.py
@@ -1,1 +1,22 @@
-print("hi")
+import tag_service_pb2_grpc
+from tag_service_pb2 import CreateClubTagRequest, GUID
+from functions import generate_guid, generate_random_string
+from requests import create_club_tag
+
+
+# Только обязательные поля
+def test_create_club_tag(grpc_channel):
+    club_guid = generate_guid()
+    name = generate_random_string(12)
+    color = generate_random_string(12)
+    stub = tag_service_pb2_grpc.TagServiceStub(grpc_channel)
+    request = CreateClubTagRequest(
+        club_guid=GUID(value=f"{club_guid}"),
+        name=name,
+        color=color)
+
+    response = stub.CreateClubTag(request)
+
+    assert len(response.tag.guid.value) == 36
+    assert response.tag.name == name
+    assert response.tag.color == color
